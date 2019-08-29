9417c101b48a7e57fe4dbade9b53701aaa8449d1
muhammedsuhademirel <msuhademirel@gmail.com>
2019-08-29 11:33:19 +0300

    RecipeBootstrap added and some modifications are made

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,14 +17,15 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
-    //todo add
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredient;
+    private Set<Ingredient> ingredient = new HashSet<>();
 
     @Lob
     private byte[] image;
@@ -33,7 +35,15 @@ public class Recipe {
 
     @ManyToMany
     @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 
     public Long getId() {
         return id;
