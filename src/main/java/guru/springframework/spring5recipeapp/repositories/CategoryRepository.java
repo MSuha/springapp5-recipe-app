52a991fec0f9ab1b1284382470a9fca042ce11e8
muhammedsuhademirel <msuhademirel@gmail.com>
2019-08-29 09:55:09 +0300

    Some Query Methods Added

--- src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -4,5 +4,10 @@ import guru.springframework.spring5recipeapp.domain.Category;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -4,5 +4,10 @@ import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
